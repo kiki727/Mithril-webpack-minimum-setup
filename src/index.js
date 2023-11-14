3165fe65ca5025eed389426d61cfcf0bc103a34e
Dejan <dejanj10@gmail.com>
2023-11-14 19:47:31 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import m from "mithril";
+
+m.render(document.body, "Hello World");
+
+console.log("Hello Webpack!");
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var config = {
+  entry: "./src", // entry point
+  output: {
+    // output folder
+    path: __dirname + "/dist",
+    filename: "bundle.js",
+  },
+};
+module.exports = config;
